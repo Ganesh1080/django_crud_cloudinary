f4ff06f0d1b08ad8f437e1bc1a39b8318312235d
Ganesh1080 <dangetisaiganesh18@gmail.com>
2025-11-07 14:52:25 +0530

    six

--- proj/settings.py
+++ proj/settings.py
@@ -13,9 +13,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import environ
 env=environ.Env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
