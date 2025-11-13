beb20d0c9260c423b2f16274e75cf090e3e92298
Ganesh1080 <dangetisaiganesh18@gmail.com>
2025-11-13 14:13:32 +0530

    chnage installed apps

--- proj/settings.py
+++ proj/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'myapp',
     'cloudinary',
     'rest_framework',
-    'corsheaders',
 ]
 # CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOWED_ORIGINS = [
@@ -73,7 +72,6 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
-    # 'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
